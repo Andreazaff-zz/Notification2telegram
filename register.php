c581304e9bdecdac30e765ab48c56399bbd67b24
Andrea Zaffignani <sicpoizn@gmail.com>
2019-08-01 21:19:22 +0200

    Upload

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://notification2telegram.herokuapp.com/execute.php';
+$BOT_TOKEN = '973004046:AAGaRD_aml_agnrA67NdsQNLsBte1NTwl50';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
